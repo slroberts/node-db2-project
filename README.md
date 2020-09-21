941942069a7a708262d183c9907cf33bbd059739
Shomari Roberts <shomariroberts@gmail.com>
2020-09-20 23:24:31 -0400

    completed stretch

--- README.md
+++ README.md
@@ -34,5 +34,6 @@ The completion of these questions is mandatory for MVP. However, passing the qui
 - Write a schema file for a `sales` table. This table should track information on the sale of each car. You may wish to research `foreign keys` in order to link each sale to the entry in `cars` which sold.
 
 ## Submission Format
-* [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
-* [ ] Fill out your module retrospective form [here](https://forms.lambdaschool.com/module-retrospective) with a link to your PR
+
+- [x] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's Repo). **Please don't merge your own pull request**
+- [x] Fill out your module retrospective form [here](https://forms.lambdaschool.com/module-retrospective) with a link to your PR
--- cars/cars-router.js
+++ cars/cars-router.js
@@ -22,6 +22,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//GET /api/cars
 router.get('/', async (req, res) => {
   try {
     const cars = await db('cars');
@@ -32,6 +33,51 @@ router.get('/', async (req, res) => {
       message: 'Error retrieving cars',
       err,
     });
+  }
+});
+
+//GET /api/cars/:id
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const car = await db.select('*').from('cars').where({ id }).first();
+    car
+      ? res.status(200).json(car)
+      : res.status(400).json({ message: 'car not found' });
+  } catch (err) {
+    res.status(500).json({ message: 'error retrieving car', err });
+  }
+});
+
+//PUT /api/cars/:id
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  try {
+    const update = await db('cars').where({ id }).update(changes);
+
+    update
+      ? res.status(200).json({ message: 'car updated' })
+      : res.status(404).json({ message: 'invalid id' });
+  } catch (err) {
+    res.status(500).json({ message: 'db problem', err });
+  }
+});
+
+//DELETE /api/cars/:id
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const count = await db.del().from('cars').where({ id });
+    count
+      ? res.status(200).json({ message: 'car deleted' })
+      : res.status(404).json({ message: 'invslid id' });
+  } catch (err) {
+    res.status(500).json({ message: 'database error', err });
   }
 });
 
